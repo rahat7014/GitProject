10d7431d20c84650a049bee1cdec7398d056776d
unknown <raqibulhasan19@gmail.com>
2019-08-08 23:29:55 +0600

    project started

---
+++ app/src/main/java/com/example/gitproject/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.gitproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    private void rahat()
+    {
+        int i = 1+1;
+    }
+}
